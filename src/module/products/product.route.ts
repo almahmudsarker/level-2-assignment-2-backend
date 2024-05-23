e13fd942ee816d2fe5adbcde1ab1dd4f75242964
almahmudsarker <almahmuds427@gmail.com>
2024-05-23 19:20:31 +0600

    product routes added

---
+++ src/module/products/product.route.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { ProductControllers } from "./product.controller";
+
+const router = express.Router();
+
+router.post("/", ProductControllers.createProduct);
+
+router.get("/", ProductControllers.getAllProducts);
+router.get("/:productId", ProductControllers.getSingleProduct);
+
+router.put("/:productId", ProductControllers.updateSingleProduct);
+
+router.delete("/:productId", ProductControllers.deleteProduct);
+
+export const ProductRoutes = router;

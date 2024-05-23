fcb7af81c30a8c91da1eba54a4a3f8a1b3472200
almahmudsarker <almahmuds427@gmail.com>
2024-05-23 19:26:06 +0600

    app updated

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
+import { ProductRoutes } from "./module/products/product.route";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/products", ProductRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello i am a server!");
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Resource not found",
+  });
 });
 
 export default app;
